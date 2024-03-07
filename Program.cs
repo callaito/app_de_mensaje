37b0ec28ab87961b2181a35945832338c658a747
callaito <radelinrsm029@gmail.com>
2024-03-07 14:05:51 -0400

    Se agrega el Proyecto

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace app_mensaje
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("PRIMER EJERCICIO");
+        }
+    }
+}
